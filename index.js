9b0de26c7bc21ab7a58cf1d6081dfab7604d242a
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-17 15:10:49 -0400

    fixed functions for menu

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const Intern = require('./lib/Intern');
 // TODO: CODE GOES HERE
 const fs = require('fs');
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
-const pageTemplate = require('./src/page-template');
-
+const generateTeam = require('./src/page-template')
 //function writeToFile(fileName, userInput) {
 //    fs.writeFile(fileName, userInput, (err) => {
 //        err ? console.error(err) : console.log('Your team profile is ready');
@@ -18,149 +16,205 @@ const pageTemplate = require('./src/page-template');
 const team = [];
 
 
-function createManager() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is the manager's name?",
-            name: 'managerName'
-        },
-        {
-            type: 'input',
-            message: "What is the manager's id?",
-            name: 'managerId'
-        },
-        {
-            type: 'input',
-            message: "What is the manager's email?",
-            name: 'managerEmail'
-        },
-        {
-            type: 'input',
-            message: "What is the manager's office number?",
-            name: 'managerOfficeNumber'
-        },
-    ]).then((data) => {
-        const manager = new Manager(
-            data.managerName,
-            data.managerId,
-            data.managerEmail,
-            data.managerOfficeNumber
-        )
+const createManager = [
+    //inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is the manager's name?",
+        name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: "What is the manager's id?",
+        name: 'managerId',
+    },
+    {
+        type: 'input',
+        message: "What is the manager's email?",
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is the manager's office number?",
+        name: 'managerOfficeNumber',
+    },
+]
+// .then((data) => {
+//    const manager = new Manager(
+//     data.managerName,
+//     data.managerId,
+//     data.managerEmail,
+//     data.managerOfficeNumber
+//   //  )
+//
+//
+//  team.push(manager);
+//writeToFile('./dist/team.html', pageTemplate(team, data));
 
-        team.push(manager);
-        writeToFile('./dist/team.html', pageTemplate(team, data));
 
-    })
-}
-createManager();
+//createManager();
 
-function createMenu() {
-    inquirer.prompt([
+const createMenu = [
+
     {
         type: 'list',
-        message: "Please choose an option",
+        message: "Please choose an option using arrow keys",
         choices: [
+            "Provide Manager information",
             "add an Engineer",
             "add an Intern",
             "finish building team"
         ],
         name: 'options'
-    }
-    ]).then((data) => {
-        let options = data.options;
-        if(options = "add an Engineer") {
-            createEngineer();
-        if(options = "add an Intern") {
-            createIntern();
-        } else { 
-            team.push(options);
-         writeToFile('./dist/team.html', pageTemplate(team, data));
-        }
+    },
+]
 
-        }
-        })
+//.then((options) => {
+//  switch (options) {
+//      case "add an Engineer":
+//          createEngineer();
+//          break;
+//      case "add an Intern":
+//          createIntern();
+//          break;
+//      default: "finish building team"
+//          fs.writeToFile('./dist/team.html', pageTemplate(team, data));
+//
+//
+//  }
 
-}
-createMenu();
+//createMenu();
 
 
 
-function createEngineer() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is the engineer's name?",
-            name: 'engineerName'
-        },
-        {
-            type: 'input',
-            message: "What is the engineer's id?",
-            name: 'engineerId'
-        },
-        {
-            type: 'input',
-            message: "What is the engineer's email?",
-            name: 'engineerEmail'
-        },
-        {
-            type: 'input',
-            message: "What is the engineer's github?",
-            name: 'engineerGithub'
-        },
-    ]).then((data) => {
-        const engineer = new Engineer(
-            data.engineerName,
-            data.engineerId,
-            data.engineerEmail,
-            data.engineerGithub
-        )
-        team.push(engineer);
-        writeToFile('./dist/team.html', pageTemplate(team, data));
-    })
-}
+//  let options = data.options;
+//  if(options = "add an Engineer") {
+//      createEngineer();
+//  if(options = "add an Intern") {
+//      createIntern();
+//  } else { 
+//      team.push(options);
+//   writeToFile('./dist/team.html', pageTemplate(team, data));
+//  }
+//
+//  }
+//})
+
+const createEngineer = [
+
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'engineerName'
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's id?",
+        name: 'engineerId'
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's email?",
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's github?",
+        name: 'engineerGithub'
+    },
+]
+//.then((data) => {
+// const engineer = new Engineer(
+//     data.engineerName,
+//     data.engineerId,
+//     data.engineerEmail,
+//     data.engineerGithub
+// )
+// team.push(engineer);
+// writeToFile('./dist/team.html', pageTemplate(team, data));
+
+
 //createEngineer();
 
-function createIntern() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is the intern's name?",
-            name: 'internName'
-        },
-        {
-            type: 'input',
-            message: "What is the intern's id?",
-            name: 'internId'
-        },
-        {
-            type: 'input',
-            message: "What is the intern's email?",
-            name: 'internEmail'
-        },
-        {
-            type: 'input',
-            message: "What is the intern's shcool?",
-            name: 'internSchool'
-        },
-    ]).then((data) => {
-        const intern = new Intern(
-            data.internName,
-            data.internId,
-            data.internEmail,
-            data.internSchool
-        )
-        team.push(intern);
-        writeToFile('./dist/team.html', pageTemplate(team, data));
-    });
+const createIntern = [
+    //inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is the intern's name?",
+        name: 'internName'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's id?",
+        name: 'internId'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's email?",
+        name: 'internEmail'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's shcool?",
+        name: 'internSchool'
+    },
+]
+// ]).then((data) => {
+//     const intern = new Intern(
+//         data.internName,
+//         data.internId,
+//         data.internEmail,
+//         data.internSchool
+//     )
+//     team.push(intern);
+//     writeToFile('./dist/team.html', pageTemplate(team, data));
+// });
+
+function menuQuestions() {
+    inquirer.prompt(createMenu)
+        .then(function ({ options }) {
+            switch (options) {
+                case "Provide Manager information":
+                    managerQuestions();
+                    break;
+                case "add an Engineer":
+                    engineerQuestions();
+                    break;
+                case "add an Intern":
+                    internQuestions();
+                    break;
+                default: "finish building team"
+                    fs.writeToFile('./dist/team.html', generateTeam(team, data));
+            }
+        })
 }
-//createIntern();
+
+function managerQuestions() {
+    inquirer.prompt(createManager)
+        .then(function ({ managerName, managerId, managerEmail, managerOfficeNumber }) {
+            team.push(Manager)
+            menuQuestions();
+
+        })
+    }
 
 
-//Prompt for if they want to add more employees or not
 
-//App menu which has choices to add
+    function init() {
+        menuQuestions()
+    }
 
-//build team function which will generate off page-template.js
+    init();
+
+
+
+
+
+        //Prompt for if they want to add more employees or not
+
+        //App menu which has choices to add
+
+        //build team function which will generate off page-template.js
+
 
 
